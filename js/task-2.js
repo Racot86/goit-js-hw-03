ca9d133a52d847826e3337cb2566feb906d41b75
dmytromayevsky <mayevskydv@gmail.com>
2024-08-06 12:28:51 +0900

    initial

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+function makeArray(firstArray, secondArray, maxLength){
+let mergedArray = firstArray.concat(secondArray);
+    mergedArray.length = maxLength;
+return mergedArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
